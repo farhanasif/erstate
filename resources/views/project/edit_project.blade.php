e5c67bcccf7c561880d1a649e0c9104ff921d243
ripon <riponahmed2201@gmail.com>
2020-07-27 02:24:05 +0600

    project form 2 column done

--- resources/views/project/edit_project.blade.php
+++ resources/views/project/edit_project.blade.php
@@ -6,124 +6,115 @@
 
 @section('content')
 
-  <div class="row">
-    <div class="col"></div>
-    <div class="col-md-10">
-        <div class="card card-success card-outline">
-            <div class="card-header">
-              <h3 class="card-title">Edit Project Information</h3>
-            </div>
-    
-            @include('message')
-    
-            <div class="card-body">
-              <!-- /.card -->
-                <!-- Horizontal Form -->
-               <form class="form-horizontal form-label-left" action="{{route('updateProject',$project->id)}}" method="post">
-                 @csrf
-    
-                   <div class="row">
-                      <div class="col-md-10 offset-1">
-                        <div class="form-group row">
-                          <label  class="col-form-label col-md-3 col-sm-3 label-align">Name</label>
-                          <div class="col-md-8 col-sm-3 ">
-                            <input type="text" name="name" class="form-control" placeholder="Name" value="{{$project->name}}">
-                            @if($errors->has('name'))
-                                <strong class="text-danger">{{ $errors->first('name') }}</strong>
-                            @endif
-                        </div>
-                        </div>
-    
-                        <div class="form-group row">
-                          <label  class="col-form-label col-md-3 col-sm-3 label-align">Description</label>
-                          <div class="col-md-8 col-sm-8 ">
-                            {{-- <input type="text" name="description" class="form-control" placeholder="Description"> --}}
-                            <textarea name="description" class="form-control" placeholder="Description" id="" cols="30" rows="3"> {{$project->description}} </textarea>
-                            @if($errors->has('description'))
-                                <strong class="text-danger">{{ $errors->first('description') }}</strong>
-                            @endif
-                        </div>
-                    </div>
+<section class="content">
+    <div class="container-fluid">
+      <!-- SELECT2 EXAMPLE -->
+      <div class="card card-default">
+        <div class="card-header bg-success">
+          <h3 class="card-title">Edit Item Information</h3>
+        </div>
 
-                          <div class="form-group row">
-                            <label  class="col-form-label col-md-3 col-sm-3 label-align">Location</label>
-                            <div class="col-md-8 col-sm-8 ">
-                                <input type="text" name="location" class="form-control" placeholder="Location" value="{{$project->location}}">
-                                @if($errors->has('location'))
-                                    <strong class="text-danger">{{ $errors->first('location') }}</strong>
-                                @endif
-                            </div>
-                          </div>
-    
-                        <div class="form-group row">
-                          <label  class="col-form-label col-md-3 col-sm-3 label-align">Facing</label>
-                          <div class="col-md-8 col-sm-8 ">
+         @include('message')
+        <!-- /.card-header -->
+        <form action="{{ route('storeItem') }}" method="POST">
+            @csrf
+            <div class="card-body">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label>Name</label>
+                      <input type="text" name="name" class="form-control" placeholder="Name" value="{{$project->name}}">
+                      @if($errors->has('name'))
+                          <strong class="text-danger">{{ $errors->first('name') }}</strong>
+                      @endif
+                    </div>
+                  </div>
+                  <!-- /.col -->
+                  <div class="col-md-6">
+                      <div class="form-group">
+                          <label>Description</label>
+                          <textarea name="description" id="description" cols="3" rows="3" class="form-control" placeholder="Description">{{$project->description}}</textarea>
+                          @if($errors->has('description'))
+                              <strong class="text-danger">{{ $errors->first('description') }}</strong>
+                          @endif                      
+                      </div>
+                  </div>
+                  <div class="col-md-6">
+                      <div class="form-group">
+                          <label>Location</label>
+                          <input type="text" name="location" class="form-control" placeholder="Location" value="{{$project->location}}">
+                          @if($errors->has('location'))
+                              <strong class="text-danger">{{ $errors->first('location') }}</strong>
+                          @endif
+                      </div>
+                  </div>
+
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Facing</label>
                             <input type="text" name="facing" class="form-control" placeholder="Facing" value="{{$project->facing}}">
                              @if($errors->has('facing'))
                                 <strong class="text-danger">{{ $errors->first('facing') }}</strong>
                              @endif
-                          </div>
-                        </div>
-    
-                        <div class="form-group row">
-                          <label for="email" class="col-form-label col-md-3 col-sm-3 label-align">Building Height</label>
-                            <div class="col-md-8 col-sm-8 ">
-                                <input type="text" name="building_height" class="form-control" placeholder="Building Height" value="{{$project->building_height}}">
-                              @if($errors->has('building_height'))
-                                <strong class="text-danger">{{ $errors->first('building_height') }}</strong>
-                              @endif
-                            </div>
-                        </div>
-    
-                        <div class="form-group row">
-                          <label for="email" class="col-form-label col-md-3 col-sm-3 label-align">Land Area</label>
-                            <div class="col-md-8 col-sm-8 ">
-                              <input type="text" class="form-control" name="land_area"  placeholder="Land Area" value="{{$project->land_area}}">
-                              @if($errors->has('land_area'))
-                                <strong class="text-danger">{{ $errors->first('land_area') }}</strong>
-                              @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-form-label col-md-3 col-sm-3 label-align">Launching Date</label>
-                              <div class="col-md-8 col-sm-8 ">
-                                <input type="date" class="form-control" name="launching_date" id="launching_date" value="{{$project->launching_date}}">
-                                @if($errors->has('launching_date'))
-                                  <strong class="text-danger">{{ $errors->first('launching_date') }}</strong>
-                                @endif
-                              </div>
-                          </div>
-
-                          <div class="form-group row">
-                            <label for="email" class="col-form-label col-md-3 col-sm-3 label-align">Hand Over Date</label>
-                              <div class="col-md-8 col-sm-8 ">
-                                <input type="date" class="form-control" name="hand_over_date" id="hand_over_date" value="{{$project->hand_over_date}}">
-                                @if($errors->has('hand_over_date'))
-                                  <strong class="text-danger">{{ $errors->first('hand_over_date') }}</strong>
-                                @endif
-                              </div>
-                          </div>
-    
-                          <div class="form-group row">
-                            <label for="email" class="col-form-label col-md-3 col-sm-3 label-align"></label>
-                              <div class="col-md-8 col-sm-8 ">
-                                <input  class="btn btn-success" type="submit" value="Submit">
-                              <a href="{{route('allProject')}}" class="btn btn-info">Back</a>
-                            </div>
-                          </div>
-                        </div>
                     </div>
-              </form>
-            <!-- /.card -->
-            </div>
-          </div>
-    </div>
-    <div class="col"></div>
-  </div>
-@endsection
+                </div>
 
-@section('custom_js')
+                <div class="col-md-6">
+                  <div class="form-group">
+                      <label>Building Height</label>
+                      <input type="text" name="building_height" class="form-control" placeholder="Building Height" value="{{$project->building_height}}">
+                      @if($errors->has('building_height'))
+                        <strong class="text-danger">{{ $errors->first('building_height') }}</strong>
+                      @endif
+                  </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group">
+                    <label>Land Area</label>
+                    <input type="text" class="form-control" name="land_area"  placeholder="Land Area" value="{{$project->land_area}}">
+                    @if($errors->has('land_area'))
+                      <strong class="text-danger">{{ $errors->first('land_area') }}</strong>
+                    @endif
+                </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="form-group">
+                  <label>Lanching Date</label>
+                  <input type="date" class="form-control" name="launching_date" id="launching_date" value="{{$project->launching_date}}">
+                  @if($errors->has('launching_date'))
+                    <strong class="text-danger">{{ $errors->first('launching_date') }}</strong>
+                  @endif
+              </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="form-group">
+                <label>Hand Over Date</label>
+                <input type="date" class="form-control" name="hand_over_date" id="hand_over_date" value="{{$project->hand_over_date}}">
+                @if($errors->has('hand_over_date'))
+                  <strong class="text-danger">{{ $errors->first('hand_over_date') }}</strong>
+                @endif
+            </div>
+        </div>
+                  <!-- /.col -->
+                </div>
+                <!-- /.row -->
+              </div>
+              <div class="card-footer">
+                 <button type="submit" class="btn btn-success ">Submit</button>
+                 <a href="{{ route('allProject') }}" type="submit" class="btn btn-info">Back</a>
+              </div>
+        </form>
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+
+  @endsection
+
+  @section('custom_js')
 
 <script>
     $(document).ready(function() {
