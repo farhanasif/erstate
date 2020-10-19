8329a96f62d296cc374d54caea0fb8d65087291e
ripon <riponahmed2201@gmail.com>
2020-10-19 23:54:30 +0600

    T AC and PL AC code added

--- resources/views/report/profit_loss_account.blade.php
+++ resources/views/report/profit_loss_account.blade.php
@@ -1,5 +1,11 @@
 @extends('master')
 @section('content')
+@section('custom_css')
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="/resources/demos/style.css">
+<link rel="stylesheet" type="text/css" media="all" href="{{ URL::to('css/report_print.css') }}" />
+
+@endsection
 
 @section('breadcrumb-title', 'Profit & Loss A/C (Income Statement)')
 
@@ -13,7 +19,7 @@
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
-                <form role="form" action="" method="get">
+                <form role="form" action="{{url('print/profit_loss/account')}}" method="get">
                     <div class="row">
                     
                     <div class="col-sm-4">
@@ -23,7 +29,7 @@
                          <select name="project_name" id="project_name" class="form-control select2bs4">  
                             <option value="">--select project name</option>
                           @foreach($projects as $project)
-                            <option value="{{ $project->name }}">{{ $project->name }}</option>
+                            <option value="{{ $project->id }}">{{ $project->name }}</option>
                           @endforeach
                         </select>  
                           </div>
@@ -33,7 +39,7 @@
                         <div class="form-group">
                         <label>&nbsp;&nbsp; From Month</label>
                           <div class="col-md-12 col-sm-12">
-                             <input type="text" class="form-control" name="from_date" placeholder="From Date For Profit & Loss" id="from_data">
+                             <input type="text" id="from_data" class="form-control" name="from_date" placeholder="From Date For Profit & Loss">
                           </div>
                         </div>
                     </div>
@@ -42,7 +48,7 @@
                         <div class="form-group">
                         <label>&nbsp;&nbsp; To Month</label>
                           <div class="col-md-12 col-sm-12">
-                             <input type="text" class="form-control" name="to_date" placeholder="To Date For Profit & Loss" id="to_data">
+                             <input type="text" id="to_data" class="form-control" name="to_date" placeholder="To Date For Profit & Loss">
                           </div>
                         </div>
                     </div>
@@ -50,32 +56,51 @@
 
                 </div>
                 <!-- /.card-body -->
-                <!-- /.card-body -->
                 <div class="card-footer">
                     <a href=""><button type="submit" id="generate" class="btn btn-success">Generate</button></a>
                 </div>
               </form>
                 <!-- /.card-footer -->
-            </div>
+            </div><br>
+            <!-- <div id="indivi" class="card card-secondary">
+              
+            </div> -->
         </section>
         <!-- /.content -->
 @endsection
-@section('customjs')
+@section('custom_js')
 
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script>
 $(document).ready(function() {
+    $( "#from_data" ).datepicker({dateFormat: 'yy-mm-dd'});
+    $( "#to_data" ).datepicker({dateFormat: 'yy-mm-dd'});
+    //$("#from_date").datepicker('show');
+  } );
 
-  $(function() {
-     $( "#from_data" ).datepicker({
-          dateFormat: "YYYY-MM-DD HH:mm:ss",
-          orientation: "bottom left"
-     });
-    $( "#to_data" ).datepicker({
-          dateFormat: "YYYY-MM-DD HH:mm:ss",
-          orientation: "bottom left"
-     });
-    });
-  });
+  // $('#to_data').change(function (e) {
+  //       e.preventDefault();
+
+  //       // var studentId=$("#studentList").val();
+  //       // var examName=$("#examName").val();
+
+  //       //console.log(classId, sectionId);
+
+  //       $.ajax({
+  //               type: "get",
+  //               url:"{{url('print/profit_loss/account')}}",
+                
+  //               success: function (data) {
+                    
+  //                   $('#indivi').html(data);
+  //               }
+  //           });
+  //      // ajax:"{{url('individual/admitCardSectionWiseList/')}}"+'/'+classId+'/'+sectionId,
+        
+  //   //table.destroy();
+
+  //   });
 </script>
 
 @endsection
